7a5a087d7b950d95569262028cfcd27e075d327f
신희성 <vvwv3092@gmail.com>
2025-04-25 10:14:11 +0900

    propagation use - 2

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @Slf4j
 @SpringBootTest
 class MemberServiceTest {
@@ -29,5 +31,23 @@ class MemberServiceTest {
         // then
         Assertions.assertTrue(memberRepository.find(username).isPresent());
         Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Tx:OFF
+     * memberRepository @Tx:ON
+     * logRepository    @Tx:ON RuntimeException
+     */
+    @Test
+    void outerTxOff_fail() {
+        // given
+        String username = "로그예외_outerTxOff_fail";
+
+        // when -> Member 저장은 커밋, Log 저장은 롤백
+        assertThatThrownBy(() -> memberService.joinV1(username));
+
+        // then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
     }
 }
